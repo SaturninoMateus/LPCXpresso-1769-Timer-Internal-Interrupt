bc59ce36fc87c7f3639217b5255cddcc089f36d6
SaturninoMateus <s.nataniel10@gmail.com>
2016-11-28 13:49:53 -0200

    initSSP implicitly

--- display.c
+++ display.c
@@ -28,6 +28,8 @@
 #include <string.h>
 #include "lpc17xx_gpio.h"
 #include "lpc17xx_i2c.h"
+
+#include "lpc17xx_pinsel.h"
 #include "lpc17xx_ssp.h"
 #include "display.h"
 #include "font5x7.h"
@@ -404,7 +406,47 @@ static void intToString(int value, uint8_t* pBuf, uint32_t len, uint32_t base)
     return;
 }
 
+
+static void initSSP(void)
+{
+	SSP_CFG_Type SSP_ConfigStruct;
+	PINSEL_CFG_Type PinCfg;
+
+	/*
+	 * Initialize SPI pin connect
+	 * P0.7 - SCK;
+	 * P0.8 - MISO
+	 * P0.9 - MOSI
+	 * P2.2 - SSEL - used as GPIO
+	 */
+	PinCfg.Funcnum = 2;
+	PinCfg.OpenDrain = 0;
+	PinCfg.Pinmode = 0;
+	PinCfg.Portnum = 0;
+	PinCfg.Pinnum = 7;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Pinnum = 8;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Pinnum = 9;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Funcnum = 0;
+	PinCfg.Portnum = 2;
+	PinCfg.Pinnum = 2;
+	PINSEL_ConfigPin(&PinCfg);
+
+	SSP_ConfigStructInit(&SSP_ConfigStruct);
+
+	// Initialize SSP peripheral with parameter given in structure above
+	SSP_Init(LPC_SSP1, &SSP_ConfigStruct);
+
+	// Enable SSP peripheral
+	SSP_Cmd(LPC_SSP1, ENABLE);
+
+}
+
+
 void display_new(){
+	initSSP();
 	oled_init();
 
 	display_clearScreen(DISPLAY_COLOR_WHITE);
--- main.c
+++ main.c
@@ -47,49 +47,9 @@
 #include "display.h"
 
 
-static void initSSP(void)
-{
-	SSP_CFG_Type SSP_ConfigStruct;
-	PINSEL_CFG_Type PinCfg;
-
-	/*
-	 * Initialize SPI pin connect
-	 * P0.7 - SCK;
-	 * P0.8 - MISO
-	 * P0.9 - MOSI
-	 * P2.2 - SSEL - used as GPIO
-	 */
-	PinCfg.Funcnum = 2;
-	PinCfg.OpenDrain = 0;
-	PinCfg.Pinmode = 0;
-	PinCfg.Portnum = 0;
-	PinCfg.Pinnum = 7;
-	PINSEL_ConfigPin(&PinCfg);
-	PinCfg.Pinnum = 8;
-	PINSEL_ConfigPin(&PinCfg);
-	PinCfg.Pinnum = 9;
-	PINSEL_ConfigPin(&PinCfg);
-	PinCfg.Funcnum = 0;
-	PinCfg.Portnum = 2;
-	PinCfg.Pinnum = 2;
-	PINSEL_ConfigPin(&PinCfg);
-
-	SSP_ConfigStructInit(&SSP_ConfigStruct);
-
-	// Initialize SSP peripheral with parameter given in structure above
-	SSP_Init(LPC_SSP1, &SSP_ConfigStruct);
-
-	// Enable SSP peripheral
-	SSP_Cmd(LPC_SSP1, ENABLE);
-
-}
-
-
 xQueueHandle Global_Queue_handle = 0;
 
 void task_reader(void *p){
-	//Read and send the data
-	initSSP();
 	Sensor_new();
 	display_new();
 	uint32_t val = 0;
